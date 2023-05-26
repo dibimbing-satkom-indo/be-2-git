72396e5da509f48959f4277089ba448daab687b8
Ifan Iqbal <ifaniqbal.com@gmail.com>
2023-05-26 13:55:05 +0700

    feat: add goroutine to loop from 0 to 4

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	fmt.Println("hello world")
+	
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		for i := 0; i < 5; i++ {
+			fmt.Println("loop:", i)
+		}
+	}()
+	wg.Wait()
 }
